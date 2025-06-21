fbfc4af0a0866d8a09f4a053ecff84abc7eb884d
azam <azoz20113040@gmail.com>
2025-06-21 17:00:25 +0300

    hj

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/fix-config', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('config:cache');
+    return 'Configuration cache cleared and rebuilt!';
 });
